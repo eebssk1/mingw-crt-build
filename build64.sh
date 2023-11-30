529879d5349de42086306cfd47009635d78f9ca7
EBK21 <chkd13303@gmail.com>
2023-11-30 19:36:13 +0000

    swtich to build on linux

--- build64.sh
+++ build64.sh
@@ -13,14 +13,16 @@ cd mingw-w64-mingw-w64
 rm -rf build
 mkdir build
 
+export PATH=$SDIR/x86_64-w64-mingw32/bin:$PATH
+
 if [ "x$(which ccache)" != "x" ]; then
-export CC="ccache gcc"
-export CXX="ccache g++"
+export CC="ccache x86_64-w64-mingw32-gcc"
+export CXX="ccache x86_64-w64-mingw32-g++"
 fi
 
-export AR="gcc-ar"
-export NM="gcc-nm"
-export RANLIB="gcc-ranlib"
+export AR="x86_64-w64-mingw32-gcc-ar"
+export NM="x86_64-w64-mingw32-gcc-nm"
+export RANLIB="x86_64-w64-mingw32-gcc-ranlib"
 
 dobuild(){
 cd build
@@ -32,10 +34,10 @@ ARCH=x86-64-v2
 ONAME=-legacy
 fi
 
-export CFLAGS="-march=$ARCH $(cat $SDIR/Z.txt) -flto=auto -ffat-lto-objects"
+export CFLAGS="-march=$ARCH $(cat $SDIR/Z.txt)"
 export CXXFLAGS="-fdeclone-ctor-dtor $CFLAGS"
 
-../configure --disable-lib32 --enable-lib64 --with-default-msvcrt=ucrt --enable-wildcard --disable-dependency-tracking --prefix=$(pwd)/out; checkreturn $?
+../configure --host=x86_64-w64-mingw32 --disable-lib32 --enable-lib64 --with-default-msvcrt=ucrt --enable-wildcard --disable-dependency-tracking --prefix=$(pwd)/out; checkreturn $?
 
 make -j3 all; checkreturn $?
 make install
@@ -47,7 +49,7 @@ export CXXFLAGS="-fdeclone-ctor-dtor $CFLAGS"
 
 rm -rf * .*
 
-../mingw-w64-libraries/winpthreads/configure --disable-dependency-tracking --prefix=$(pwd)/out --disable-shared; checkreturn $?
+../mingw-w64-libraries/winpthreads/configure --host=x86_64-w64-mingw32 --disable-dependency-tracking --prefix=$(pwd)/out --disable-shared; checkreturn $?
 
 make -j3 all; checkreturn $?
 make install
@@ -57,7 +59,7 @@ make distclean
 export CFLAGS="-march=$ARCH $(cat $SDIR/f2.txt)"
 export CXXFLAGS="$CFLAGS"
 
-../mingw-w64-libraries/winpthreads/configure --disable-dependency-tracking --prefix=$(pwd)/out --disable-static; checkreturn $?
+../mingw-w64-libraries/winpthreads/configure --host=x86_64-w64-mingw32 --disable-dependency-tracking --prefix=$(pwd)/out --disable-static; checkreturn $?
 
 make -j3 all; checkreturn $?
 make install
@@ -68,12 +70,12 @@ rm -rf * .*
 
 cd ..
 
-ar rcs out/lib/libssp.a
-ar rcs out/lib/libssp_nonshared.a
+x86_64-w64-mingw32-ar rcs out/lib/libssp.a
+x86_64-w64-mingw32-ar rcs out/lib/libssp_nonshared.a
 
 mv out ../ucrt64$ONAME
 
-cp -a /ucrt64/lib/default-manifest.o ../ucrt64$ONAME/lib/
+cp -a $SDIR/default-manifest_64.o ../ucrt64$ONAME/lib/default-manifest.o
 }
 
 dobuild legacy

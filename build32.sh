8bd8c4c9f37cffd9cab294d9d7a741dad63dafb0
EBK21 <chkd13303@gmail.com>
2024-09-30 15:54:53 +0000

    update cpu target script maintenance cflags maintenance

--- build32.sh
+++ build32.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-checkreturn(){
-  if [ $1 -ne 0 ]; then
-    exit $1
-  fi
-}
-
 export SDIR=$PWD
 
 cd mingw-w64-mingw-w64
@@ -28,30 +22,30 @@ export CXX="ccache $CXX"
 fi
 
 
-export AR="x86_64-w64-mingw32-gcc-ar"
-export NM="x86_64-w64-mingw32-gcc-nm"
+export AR="x86_64-w64-mingw32-gcc-ar --target=pe-i386"
+export NM="x86_64-w64-mingw32-gcc-nm --target=pe-i386"
 export RANLIB="x86_64-w64-mingw32-gcc-ranlib"
 
-export CFLAGS="-march=prescott @${SDIR}/Z.txt @${SDIR}/opt.txt"
+export CFLAGS="-march=westmere @${SDIR}/Z.txt @${SDIR}/opt.txt"
 export CXXFLAGS="-fdeclone-ctor-dtor $CFLAGS"
 
 export CPPFLAGS="-Wno-expansion-to-defined -I$PWD/../mingw-w64-headers/crt  -I$PWD/../mingw-w64-headers/include"
 
-../configure --host=x86_64-w64-mingw32 --enable-lib32 --disable-lib64 --with-default-msvcrt=ucrt --enable-wildcard --with-libraries=pseh --disable-dependency-tracking --prefix=$(pwd)/out; checkreturn $?
+../configure --host=x86_64-w64-mingw32 --enable-lib32 --disable-lib64 --with-default-msvcrt=ucrt --enable-wildcard --with-libraries=pseh --disable-dependency-tracking --prefix=$(pwd)/out || exit 255
 
-make -j3 all; checkreturn $?
+make -j3 all || exit 255
 make install
 
 mv out ../
 
-export CFLAGS="-march=prescott @${SDIR}/f.txt @${SDIR}/opt.txt"
+export CFLAGS="-march=westmere @${SDIR}/f.txt @${SDIR}/opt.txt"
 export CXXFLAGS="-fdeclone-ctor-dtor $CFLAGS"
 
 rm -rf * .*
 
-RC="x86_64-w64-mingw32-windres -F pe-i386" CC="x86_64-w64-mingw32-gcc -m32" CXX="x86_64-w64-mingw32-g++ -m32"  ../mingw-w64-libraries/winpthreads/configure --host=i686-w64-mingw32 --disable-dependency-tracking --prefix=$(pwd)/out; checkreturn $?
+RC="x86_64-w64-mingw32-windres -F pe-i386" CC="x86_64-w64-mingw32-gcc -m32" CXX="x86_64-w64-mingw32-g++ -m32"  ../mingw-w64-libraries/winpthreads/configure --host=i686-w64-mingw32 --disable-dependency-tracking --prefix=$(pwd)/out || exit 255
 
-make -j3 all; checkreturn $?
+make -j3 all || exit 255
 make install
 
 cp -a out/. ../out/
@@ -60,8 +54,8 @@ rm -rf * .*
 
 cd ..
 
-x86_64-w64-mingw32-ar --target=pe-i386 rcs out/lib/libssp.a
-x86_64-w64-mingw32-ar --target=pe-i386 rcs out/lib/libssp_nonshared.a
+#x86_64-w64-mingw32-ar --target=pe-i386 rcs out/lib/libssp.a
+#x86_64-w64-mingw32-ar --target=pe-i386 rcs out/lib/libssp_nonshared.a
 
 mv out ../msvcrt32
 
--- build64.sh
+++ build64.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-checkreturn(){
-  if [ $1 -ne 0 ]; then
-    exit $1
-  fi
-}
-
 export SDIR=$PWD
 
 cd mingw-w64-mingw-w64
@@ -27,24 +21,21 @@ export RANLIB="x86_64-w64-mingw32-gcc-ranlib"
 dobuild(){
 cd build
 
-ARCH=x86-64-v3
+ARCH=ivybridge
 ONAME=
 if [ "$1" = "legacy" ]; then
-ARCH=ivybridge
+ARCH=westmere
 ONAME=-legacy
 fi
-if [ "$1" = "legacy_super" ]; then
-ARCH=core2
-ONAME=-legacy_super
-fi
+
 export CFLAGS="-march=$ARCH @${SDIR}/Z.txt @${SDIR}/opt.txt"
 export CXXFLAGS="-fdeclone-ctor-dtor $CFLAGS"
 
 export CPPFLAGS="-Wno-expansion-to-defined -I$PWD/../mingw-w64-headers/crt  -I$PWD/../mingw-w64-headers/include"
 
-../configure --host=x86_64-w64-mingw32 --disable-lib32 --enable-lib64 --with-default-msvcrt=ucrt --enable-wildcard --disable-dependency-tracking --prefix=$(pwd)/out; checkreturn $?
+../configure --host=x86_64-w64-mingw32 --disable-lib32 --enable-lib64 --with-default-msvcrt=ucrt --enable-wildcard --disable-dependency-tracking --prefix=$(pwd)/out || exit 255
 
-make -j3 all; checkreturn $?
+make -j3 all || exit 255
 make install
 
 mv out ../
@@ -54,9 +45,9 @@ export CXXFLAGS="-fdeclone-ctor-dtor $CFLAGS"
 
 rm -rf * .*
 
-../mingw-w64-libraries/winpthreads/configure --host=x86_64-w64-mingw32 --disable-dependency-tracking --prefix=$(pwd)/out; checkreturn $?
+../mingw-w64-libraries/winpthreads/configure --host=x86_64-w64-mingw32 --disable-dependency-tracking --prefix=$(pwd)/out || exit 255
 
-make -j3 all; checkreturn $?
+make -j3 all || exit 255
 make install
 
 cp -a out/. ../out/
@@ -65,15 +56,14 @@ rm -rf * .*
 
 cd ..
 
-x86_64-w64-mingw32-ar rcs out/lib/libssp.a
-x86_64-w64-mingw32-ar rcs out/lib/libssp_nonshared.a
+#x86_64-w64-mingw32-ar rcs out/lib/libssp.a
+#x86_64-w64-mingw32-ar rcs out/lib/libssp_nonshared.a
 
 mv out ../ucrt64$ONAME
 
 cp -a $SDIR/default-manifest_64.o ../ucrt64$ONAME/lib/default-manifest.o
 }
 
-dobuild legacy_super
 dobuild legacy
 dobuild
 
